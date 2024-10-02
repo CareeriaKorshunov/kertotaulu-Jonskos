e0eb9f50270c4b67e54141ff8f073b4f77d4b05a
Jonskos <joonas.melvasalo@protonmail.com>
2024-10-02 10:33:46 +0300

    Add program

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -2,10 +2,18 @@
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            //Koodi tähän
-          
+        public static void Main(string[] args) {
+            int luku;
+            Console.Write("Anna luku: ");
+            while(!int.TryParse(Console.ReadLine(), out luku)) {
+                Console.Write("Anna luku: ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"Antamasi luvun ({luku}) kertotaulu:");
+            for (int i = 1; i < 11; i++) {
+                Console.WriteLine($"{luku} * {i} = {luku * i}");
+            }
 
         }
 
